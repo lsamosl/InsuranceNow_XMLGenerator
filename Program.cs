9e2631228f5dec620ae7b19d3bb8cf14adf0c724
Luis Samos <lsamosl.net@gmail.com>
2020-05-07 10:53:00 -0500

    Basic XML generation

--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace InsuranceNow_XMLGenerator
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string fileName = "InsuranceNow_" + Guid.NewGuid().ToString() + ".xml";
+                string path = "C:\\Test\\" + fileName;
+                XMlGenerator Generator = new XMlGenerator(path);
+                Generator.Generate();
+                Console.WriteLine("Done!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+            
         }
     }
 }
--- XMlGenerator.cs
+++ XMlGenerator.cs
@@ -19,13 +19,89 @@ namespace InsuranceNow_XMLGenerator
 
         public void Generate()
         {
-            XmlWriterSettings settings = new XmlWriterSettings();
-            settings.Indent = true;
-            settings.OmitXmlDeclaration = true;
-
-            using (XmlWriter writer = XmlWriter.Create(Path, settings))
+            try
             {
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.OmitXmlDeclaration = true;
 
+                using (XmlWriter writer = XmlWriter.Create(Path, settings))
+                {
+                    writer.WriteStartDocument();
+
+                    writer.WriteStartElement("DTORoot");
+
+                    #region Header
+
+                    writer.WriteStartElement("DTOApplication");
+                    writer.WriteAttributeString("Version", "3.10");
+                    writer.WriteAttributeString("Status", "In Process");
+                    writer.WriteAttributeString("TypeCd", "Application");
+                    writer.WriteAttributeString("Description", "New Application");
+                    writer.WriteAttributeString("ReadyToRateInd", "Yes");
+
+                    #endregion
+
+                    #region QuestionReplies
+
+                    writer.WriteStartElement("QuestionReplies");
+                    writer.WriteAttributeString("QuestionSourceMDA", "UWProduct::product-master::waic-CA-PersonalAuto-v01-00-01:://ProductSetup[@id='ProductSetup']");
+
+                    writer.WriteStartElement("QuestionReply");
+                    writer.WriteAttributeString("Name", "UWQuestionFraud");
+                    writer.WriteAttributeString("Value", "NO");
+                    writer.WriteAttributeString("VisibleInd", "YES");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("QuestionReply");
+                    writer.WriteAttributeString("Name", "UWQuestionVehicleSharing");
+                    writer.WriteAttributeString("Value", "NO");
+                    writer.WriteAttributeString("VisibleInd", "YES");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("QuestionReply");
+                    writer.WriteAttributeString("Name", "UWPublicDelivery");
+                    writer.WriteAttributeString("Value", "NO");
+                    writer.WriteAttributeString("VisibleInd", "YES");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("QuestionReply");
+                    writer.WriteAttributeString("Name", "UWQuestionOtherDelivery");
+                    writer.WriteAttributeString("Value", "NO");
+                    writer.WriteAttributeString("VisibleInd", "YES");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("QuestionReply");
+                    writer.WriteAttributeString("Name", "UWQuestionGaragingLocation");
+                    writer.WriteAttributeString("Value", "NO");
+                    writer.WriteAttributeString("VisibleInd", "YES");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("QuestionReply");
+                    writer.WriteAttributeString("Name", "UWQuestionOtherVehicles");
+                    writer.WriteAttributeString("Value", "NO");
+                    writer.WriteAttributeString("VisibleInd", "YES");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("QuestionReply");
+                    writer.WriteAttributeString("Name", "UWQuestionHousehold");
+                    writer.WriteAttributeString("Value", "NO");
+                    writer.WriteAttributeString("VisibleInd", "YES");
+                    writer.WriteEndElement();
+
+                    writer.WriteEndElement();
+
+                    #endregion
+
+                    writer.WriteEndElement();
+                    writer.WriteEndElement();
+
+                    writer.WriteEndDocument();
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
             }
         }
     }
