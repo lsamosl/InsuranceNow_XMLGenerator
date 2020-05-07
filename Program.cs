12fca8707ed251479af39dd099e26fd2736fd99f
Luis Samos <lsamosl.net@gmail.com>
2020-05-07 10:28:27 -0500

    Project Initialization

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceNow_XMLGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ XMlGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace InsuranceNow_XMLGenerator
+{
+    public class XMlGenerator
+    {
+        public XMlGenerator(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; set; }
+
+        public void Generate()
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.OmitXmlDeclaration = true;
+
+            using (XmlWriter writer = XmlWriter.Create(Path, settings))
+            {
+
+            }
+        }
+    }
+}
