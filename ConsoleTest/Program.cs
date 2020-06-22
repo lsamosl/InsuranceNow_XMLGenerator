16ce1409ef01cda08b32932c96cbb4bfc22f41a9
Miguel Angel Pat Lopez (Contractor) <MPLopez@waic.com>
2020-06-22 10:37:43 -0500

    parametrizacion y reconocimiento de excel automatico

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -4,7 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO.Compression;
 using System.IO;
-//using System.IO.Compression.FileSystem;
+using System.IO.Directory;
+using System.Configuration;
 using XMLParser;
 
 namespace ConsoleTest
@@ -15,11 +16,18 @@ namespace ConsoleTest
         {
             try
             {
-                string path = "C:\\Test\\";
+                string xmlPath = Convert.ToString(ConfigurationManager.AppSettings.Get("xmlPath"));
+                string zipsPath = Convert.ToString(ConfigurationManager.AppSettings.Get("zipsPath"));
+                string excelPath = Convert.ToString(ConfigurationManager.AppSettings.Get("excelPath"));
+                string emptyPath = Convert.ToString(ConfigurationManager.AppSettings.Get("emptyPath"));
+
+                //string path = "C:\\Test\\";
                 string XmlOutput = "InsuranceNow_[POLICYNUMBER]_Drivers_[DRIVERS]_Vehicles_[VEHICLES].xml";
-                string ExcelInput = "dataMigrationVer1.6.xlsm";
-                string zipPath = string.Empty;
+                string[] files = Directory.GetFiles(excelPath, "*.xls", SearchOption.AllDirectories);
+
+                //string ExcelInput = "dataMigrationVer1.6.xlsm";
                 string zipFullPath = string.Empty;
+                string zipName = string.Empty;
                 List<Policy> Policies = new List<Policy>();
                 List<string> XmlFileNames = new List<string>();
                 int total = 1;
@@ -27,7 +35,7 @@ namespace ConsoleTest
                 int zipsCreated = 1;
 
                 Console.WriteLine("Processing excel file...");
-                ExcelUtil excelUtil = new ExcelUtil(path + ExcelInput);
+                ExcelUtil excelUtil = new ExcelUtil(excelPath);
                 var workBook = excelUtil.OpenFile();
                 excelUtil.ProcessFile(workBook, Policies);
                 excelUtil.CloseFile(workBook);
@@ -41,7 +49,7 @@ namespace ConsoleTest
                                                 Replace("[VEHICLES]", p.Vehicles.Count.ToString());
 
                     XmlFileNames.Add(fileName);
-                    XMLGenerator Generator = new XMLGenerator(path + "XMLs\\" + fileName, p);
+                    XMLGenerator Generator = new XMLGenerator(xmlPath + fileName, p);
                     Generator.Generate();
                     Console.Write("\r{0}  ", total);
                     total++;
@@ -55,19 +63,18 @@ namespace ConsoleTest
                     
                     if (zippedFiles % 10 == 0)
                     {                        
-                        string startPath = path + "Empty\\";
-                        zipPath = "InsuranceNow_" + zipsCreated.ToString() + ".zip";
-                        zipFullPath = "C:\\Test\\Zips\\" + zipPath;
-                        //File.Delete(zipFullPath);
-                        ZipFile.CreateFromDirectory(startPath, zipFullPath);                        
+                        zipName = "InsuranceNow_" + zipsCreated.ToString() + ".zip";
+                        zipFullPath = zipsPath + zipName;                        
+                        ZipFile.CreateFromDirectory(emptyPath, zipFullPath);                        
                         zipsCreated++;
                     }
 
                     using (ZipArchive zip = ZipFile.Open(zipFullPath, ZipArchiveMode.Update))
                     {
-                        string filePath = "C:\\Test\\XMLs\\" + xmlFileName;
+                        string filePath = xmlPath + xmlFileName;
                         zip.CreateEntryFromFile(filePath, xmlFileName);
                         zippedFiles++;
+                        File.Delete(filePath);
                     }                    
                 }
                 
